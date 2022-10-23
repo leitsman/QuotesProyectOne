0d7581a33c6cc3f544fbd705b892057fd1df0925
Emmanuel C <misterium273@gmail.com>
2022-10-22 23:48:33 -0400

    Create commit, ready to upload at netlify with archivements base

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import QuoteBox from "./Components/QuoteBox";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <QuoteBox />
+    </div>
+  );
+}
+
+export default App;
