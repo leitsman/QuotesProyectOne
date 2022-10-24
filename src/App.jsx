d35d87bc4174ae7e632c7fad4cb5f778d1372183
Emmanuel C <misterium273@gmail.com>
2022-10-24 12:04:45 -0400

    this version includes: -quotes -rgb random to click button -CardQuotes center no includes: -responsive on phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import QuoteBox from "./Components/QuoteBox";
+import quotes from "../src/quotes.json";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  // ===================== RANDOM INDEX =====================
+  const IndexRndm = Math.floor(Math.random() * (quotes.length - 1));
+  const [RandomIndx, setRandomIndx] = useState(IndexRndm);
+  const BtnRandom = () => {
+    const OtherRndm = Math.floor(Math.random() * (quotes.length - 1));
+    setRandomIndx(OtherRndm);
+  };
+  // ====================== RANDOM RGB ==================
+  const RandomRgb = () => {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  };
   return (
     <div className="App">
-      <QuoteBox />
+      <QuoteBox
+        BtnRandom={BtnRandom}
+        RandomIndx={RandomIndx}
+        RandomRgb={RandomRgb}
+      />
     </div>
   );
 }
--- src/Components/QuoteBox.jsx
+++ src/Components/QuoteBox.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Quotes from "../quotes.json";
 
-const QuoteBox = () => {
-  return <div></div>;
+const QuoteBox = ({ BtnRandom, RandomIndx, RandomRgb }) => {
+  let ReceiveRgb = RandomRgb();
+  document.body.style = `background:${ReceiveRgb}`;
+  return (
+    <div className="Content--Quote-card" style={{ color: ReceiveRgb }}>
+      <div className="Content--fhrase-marks">
+        <i className="fa-solid fa-marker fa-3x"></i>{" "}
+        <p>{Quotes[RandomIndx].quote}</p>
+      </div>
+      <p>{Quotes[RandomIndx].author}</p>
+      <button
+        className="button--Next-Quote"
+        onClick={BtnRandom}
+        style={{ color: ReceiveRgb }}
+      >
+        <i className="fa-solid fa-angles-right"></i>
+      </button>
+    </div>
+  );
 };
 
 export default QuoteBox;
