61b7fcdc01f9273923792593beca11aec2fe097f
Emmanuel C <misterium273@gmail.com>
2023-06-05 12:06:22 -0400

    perf: se remodelo a una nueva version la pagina incluye triada de color

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import QuoteBox from "./Components/QuoteBox";
-import quotes from "../src/quotes.json";
 
 function App() {
-  // ===================== RANDOM INDEX =====================
-  const IndexRndm = Math.floor(Math.random() * (quotes.length - 1));
-  const [RandomIndx, setRandomIndx] = useState(IndexRndm);
-  const BtnRandom = () => {
-    const OtherRndm = Math.floor(Math.random() * (quotes.length - 1));
-    setRandomIndx(OtherRndm);
-  };
-  // ====================== RANDOM RGB ==================
-  const RandomRgb = () => {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-  };
   return (
     <div className="App">
-      <QuoteBox
-        BtnRandom={BtnRandom}
-        RandomIndx={RandomIndx}
-        RandomRgb={RandomRgb}
-      />
+      <QuoteBox />
     </div>
   );
 }
--- src/Components/QuoteBox.jsx
+++ src/Components/QuoteBox.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Quotes from "../quotes.json";
 
-const QuoteBox = ({ BtnRandom, RandomIndx, RandomRgb }) => {
-  let ReceiveRgb = RandomRgb();
-  document.body.style = `background:${ReceiveRgb}`;
+const QuoteBox = () => {
+  const randomIndex = Math.floor(Math.random() * Quotes.length - 1);
+  const [index, setIndex] = useState(Math.abs(randomIndex));
+  const randomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * Quotes.length - 1);
+    setIndex(Math.abs(randomIndex));
+  };
+  //? color
+  const matiz = Math.floor(Math.random() * 360);
+  const saturation = Math.floor(Math.random() * 100);
+  const lightNessText = Math.floor(Math.random() * 50) + 30;
+  const lightNess = Math.floor(Math.random() * 60);
+  document.body.style.background = `hsl(${matiz}, ${saturation}%, ${lightNess}%)`;
   return (
-    <div className="Content--Quote-card" style={{ color: ReceiveRgb }}>
-      <div className="Content--fhrase-marks">
-        <i className="fa-solid fa-marker fa-3x"></i>{" "}
-        <p>{Quotes[RandomIndx].quote}</p>
+    <div className=" bg-white mx-3 mt-[30vh] px-3 py-5 flex flex-col items-center shadow-xl shadow-black sm:max-w-[700px] sm:mx-auto">
+      {/* <h1 className=" text-2xl font-sans mb-5">Do you prefer?</h1> */}
+      <div className="flex items-center justify-evenly mb-5 gap-3">
+        {/* <button className="border-4 rounded-sm border-double active:border-solid flex items-center gap-2 px-2 py-2">
+          <i className="fa-regular fa-money-bill-1 fa-2x"></i>1 Dolar
+        </button>
+        <span>OR</span> */}
+        <button
+          className="border-4 rounded border-double border-orange-600 active:border-solid flex gap-2 px-2 items-center active:text-orange-600"
+          onClick={randomQuote}
+        >
+          <i className="fa-regular fa-thumbs-up fa-2x"></i>
+          <span className="text-lg">
+            consejo <br /> de vida
+          </span>
+        </button>
       </div>
-      <p>{Quotes[RandomIndx].author}</p>
-      <button
-        className="button--Next-Quote"
-        onClick={BtnRandom}
-        style={{ color: ReceiveRgb }}
+      <p
+        style={{
+          color: `hsl(${matiz + 120}, ${saturation}%, ${lightNessText}%)`,
+        }}
       >
-        <i className="fa-solid fa-angles-right"></i>
-      </button>
+        {Quotes[index].quote}
+      </p>
+      <div className=" flex items-center self-end mt-5 mr-4 gap-2">
+        <p>-{Quotes[index].author}</p>
+        <i
+          className="fa-solid fa-marker fa-2x"
+          style={{
+            color: `hsl(${matiz + 240}, ${saturation}%, ${lightNessText}%)`,
+          }}
+        ></i>
+      </div>
     </div>
   );
 };
